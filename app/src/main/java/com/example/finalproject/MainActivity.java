5b1268a2464c758e552b414a11e06369653c8111
Shayan Khalkhali <shayank97@hotmail.com>
2022-10-26 21:05:12 -0400

    added RecyclerView to main for now but will change to a different page later.

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,30 +1,46 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.finalproject.database.StudyRoomDB;
 import com.example.finalproject.database.entity.Sensor;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    protected RecyclerView RoomRecyclerView;
+    protected RoomRecyclerViewAdapter RoomRecyclerViewAdapter;
+    protected StudyRoomDB studyRoomDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        StudyRoomDB db = StudyRoomDB.getInstance(getApplicationContext());
+        studyRoomDB = StudyRoomDB.getInstance(this);
 
-        // Just testing can delete.
-        for(int roomNumber = 1; roomNumber <= 10; roomNumber++) {
-            for(int floorNumber = 1; floorNumber <= 10; floorNumber++) {
-                String roomID = "ID: " + roomNumber + floorNumber;
-                String roomLocation = "Room " + roomNumber + ", Floor " + 1;
-                String roomStatus = "Available";
-                Sensor sensor = new Sensor(roomNumber, roomID, roomLocation, roomStatus);
-                db.sensorDao().insertAll(sensor);
-            }
-            }
-        }
+        String roomName = "Room " + (int) (Math.random() * 100);
+        String roomLocation = "John Molson School of Business";
+        String roomStatus = "Available";
+        Sensor sensor = new Sensor(0, roomName, roomLocation, roomStatus);
+        studyRoomDB.sensorDao().insertAll(sensor);
+
+
+        setupRecyclerView();
     }
+
+    protected void setupRecyclerView() {
+        List<Sensor> RoomList = studyRoomDB.sensorDao().getAll();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        RoomRecyclerViewAdapter = new RoomRecyclerViewAdapter(RoomList);
+
+        RoomRecyclerView = findViewById(R.id.Sensor_RecyclerViewID);
+        RoomRecyclerView.setLayoutManager(linearLayoutManager);
+        RoomRecyclerView.setAdapter(RoomRecyclerViewAdapter);
+    }
+}
