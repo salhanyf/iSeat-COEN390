8f7659e2c99a2e2e2f244e5f9dea95f388ec7ad6
Shayan Khalkhali <shayank97@hotmail.com>
2022-10-27 14:10:58 -0400

    renamed some files and moved some code from mainactivity to proper place

--- app/src/main/java/com/example/finalproject/SeatsActivityAdmin.java
+++ app/src/main/java/com/example/finalproject/SeatsActivityAdmin.java
@@ -1,16 +1,14 @@
 package com.example.finalproject;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class SeatsActivityAdmin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_seats_admin);
     }
-
 }
---
+++ app/src/main/java/com/example/finalproject/SeatsActivityUser.java
@@ -0,0 +1,44 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.finalproject.database.StudyRoomDB;
+import com.example.finalproject.database.entity.Sensor;
+
+import java.util.List;
+
+public class SeatsActivityUser extends AppCompatActivity {
+    protected RecyclerView RoomRecyclerView;
+    protected RoomRecyclerViewAdapter RoomRecyclerViewAdapter;
+    protected StudyRoomDB studyRoomDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        studyRoomDB = StudyRoomDB.getInstance(this);
+
+        String roomName = "Room " + (int) (Math.random() * 100);
+        String roomLocation = "John Molson School of Business";
+        String roomStatus = "Available";
+        Sensor sensor = new Sensor(0, roomName, roomLocation, roomStatus);
+        studyRoomDB.sensorDao().insertAll(sensor);
+
+        setupRecyclerView();
+    }
+
+    protected void setupRecyclerView() {
+        List<Sensor> RoomList = studyRoomDB.sensorDao().getAll();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        RoomRecyclerViewAdapter = new RoomRecyclerViewAdapter(RoomList);
+
+        RoomRecyclerView = findViewById(R.id.Sensor_RecyclerViewID);
+        RoomRecyclerView.setLayoutManager(linearLayoutManager);
+        RoomRecyclerView.setAdapter(RoomRecyclerViewAdapter);
+    }
+}
