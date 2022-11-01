36177213202625060471e5850e01f7a58242e58f
SamsonKaller <samson.kaller@gmail.com>
2022-10-31 23:55:17 -0400

    Fix bugs caused by merge.

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
 //        StudyRoomDB db = StudyRoomDB.getInstance(getApplicationContext());
 
@@ -27,7 +26,5 @@ public class MainActivity extends AppCompatActivity {
                 db.sensorDao().insertAll(sensor);
             }
             }*/
-        }
     }
-
 }
