4398dbd62d742bf8856f0e7ae09ef8eeaf22f134
SamsonKaller <samson.kaller@gmail.com>
2022-11-02 17:23:47 -0400

    Added test buttons for demo

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: check logged-in status and start login activity if not
-//        startActivity(new Intent(this, LoginActivity.class));
+        if (FirebaseAuth.getInstance().getCurrentUser() == null)
+            startActivity(new Intent(this, LoginActivity.class));
 
-        addRemoveSensorsTestButtons();
+        // TEST BUTTONS FOR DEMO:
+        signOutTestButton();            // allows signed-in user to sign out
+        seatsActivityTestButton();      // goes to seat activity
+        addRemoveSensorsTestButtons();  // adds or removes sensor from room 1
 
         /*
         // Just testing can delete.
@@ -30,6 +37,46 @@ public class MainActivity extends AppCompatActivity {
                 db.sensorDao().insertAll(sensor);
             }
             }*/
+    }
+
+    private void signOutTestButton() {
+        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.topToTop = R.id.constraintLayoutMainActivity;
+        params.endToEnd = R.id.constraintLayoutMainActivity;
+        params.topMargin = 25;
+        params.rightMargin = 25;
+        Button button = new Button(this);
+        button.setLayoutParams(params);
+        button.setBackgroundColor(getColor(R.color.white));
+        button.setTextColor(0xFF0000FF);
+        button.setText("  Sign out  ");
+        button.setOnClickListener(view -> {
+            if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                String username = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(this, "User: " + username + " logged off.", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, LoginActivity.class));
+            }
+        });
+        ((ConstraintLayout) findViewById(R.id.constraintLayoutMainActivity)).addView(button);
+    }
+
+    private void seatsActivityTestButton() {
+        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.startToStart = R.id.constraintLayoutMainActivity;
+        params.endToEnd = R.id.constraintLayoutMainActivity;
+        params.topToTop = R.id.constraintLayoutMainActivity;
+        params.topMargin = 250;
+        Button button = new Button(this);
+        button.setLayoutParams(params);
+        button.setBackgroundColor(getColor(R.color.purple_700));
+        button.setTextColor(getColor(R.color.white));
+        button.setText("  Goto Seats Activity  ");
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SeatsActivityUser.class);
+            startActivity(intent);
+        });
+        ((ConstraintLayout) findViewById(R.id.constraintLayoutMainActivity)).addView(button);
     }
 
     private void addRemoveSensorsTestButtons() {
