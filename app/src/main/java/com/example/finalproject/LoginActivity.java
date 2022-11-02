548901d5acb68f9c5eb6f39225be4fd15cb3a3c4
SamsonKaller <samson.kaller@gmail.com>
2022-11-02 17:53:46 -0400

    changed finishAffinity() back to finish(), the fix did not work. Will have to debug. #bug

--- app/src/main/java/com/example/finalproject/LoginActivity.java
+++ app/src/main/java/com/example/finalproject/LoginActivity.java
@@ -46,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
                             public void onSuccess(AuthResult authResult) {
                                 Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                                finishAffinity();
+                                finish();
                             }
                         }).addOnFailureListener(new OnFailureListener() {
                             @Override
