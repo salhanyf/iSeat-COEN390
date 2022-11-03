3aed90d3900140fe63ca9a70b890f8dba369a41f
SamsonKaller <samson.kaller@gmail.com>
2022-11-03 16:47:42 -0400

    Coded .java view for AdminRoomsActivity, for now dummy rooms, update to Firebase read soon

--- app/src/main/java/com/example/finalproject/views/AdminRoomsActivity.java
+++ app/src/main/java/com/example/finalproject/views/AdminRoomsActivity.java
@@ -3,23 +3,42 @@ package com.example.finalproject.views;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.finalproject.R;
+import com.example.finalproject.models.Room;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminRoomsActivity extends AppCompatActivity {
+
+    private RecyclerView recycler;
+    private AdminRoomsRecyclerViewAdaptor adaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_rooms);
 
+        Intent intent = getIntent();
+        String adminEmail = intent.getStringExtra(getString(R.string.Extra_adminEmail));
+
         Toolbar toolbar = findViewById(R.id.adminRoomsActivityToolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle(String.format(getString(R.string.AdminRooms_Toolbar_Tile), "Samson"));
+        toolbar.setTitle(String.format(getString(R.string.AdminRooms_Toolbar_Tile), adminEmail != null ? adminEmail : ""));
+
+        List<Room> r = new ArrayList<>();
+        r.add(new Room("H-404", "Hall Building", "0/0", "test@mail.com"));
+        r.add(new Room("FB-C080", "Fauxbourg Building OMG OMG OMG", "0/0", "test@mail.com"));
+        r.add(new Room("LB-200", "Library Building", "0/0", "test@mail.com"));
+        setupRecyclerView(r);
     }
 
     @Override
@@ -32,13 +51,18 @@ public class AdminRoomsActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_add_room) {
-
             return true;
         }
         else if (id == R.id.action_remove_room) {
-
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupRecyclerView(List<Room> rooms) {
+        adaptor = new AdminRoomsRecyclerViewAdaptor(rooms);
+        recycler = findViewById(R.id.adminRoomsActivityRecyclerView);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setAdapter(adaptor);
     }
 }
