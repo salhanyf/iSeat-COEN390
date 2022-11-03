fd9e32111cc27255760063d6af5d01f3fe3e4fba
SamsonKaller <samson.kaller@gmail.com>
2022-11-03 12:38:23 -0400

    AdminRooms: added toolbar with title and menu buttons with icons

--- app/src/main/java/com/example/finalproject/views/AdminRoomsActivity.java
+++ app/src/main/java/com/example/finalproject/views/AdminRoomsActivity.java
@@ -1,8 +1,12 @@
 package com.example.finalproject.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.finalproject.R;
 
@@ -12,5 +16,29 @@ public class AdminRoomsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_rooms);
+
+        Toolbar toolbar = findViewById(R.id.adminRoomsActivityToolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(String.format(getString(R.string.AdminRooms_Toolbar_Tile), "Samson"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_admin_rooms, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_add_room) {
+
+            return true;
+        }
+        else if (id == R.id.action_remove_room) {
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
