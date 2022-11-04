fc84f0f6111fbf63580387d1e014792ee3c22713
SamsonKaller <samson.kaller@gmail.com>
2022-11-04 19:18:04 -0400

    AdminRooms RV Adaptor: added FragmentManager to constructor for DialogFragments, and made +/- buttons open add/remove sensor dialog fragments

--- app/src/main/java/com/example/finalproject/views/AdminRoomsRecyclerViewAdaptor.java
+++ app/src/main/java/com/example/finalproject/views/AdminRoomsRecyclerViewAdaptor.java
@@ -7,6 +7,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.finalproject.R;
@@ -15,9 +16,13 @@ import com.example.finalproject.models.Room;
 import java.util.List;
 
 public class AdminRoomsRecyclerViewAdaptor extends RecyclerView.Adapter<AdminRoomsRecyclerViewAdaptor.ViewHolder> {
+    private FragmentManager fragmentManager;
     private List<Room> localDatSet;
 
-    public AdminRoomsRecyclerViewAdaptor(List<Room> localDatSet) { this.localDatSet = localDatSet; }
+    public AdminRoomsRecyclerViewAdaptor(List<Room> localDatSet, FragmentManager fragmentManager) {
+        this.localDatSet = localDatSet;
+        this.fragmentManager = fragmentManager;
+    }
 
     @NonNull
     @Override
@@ -29,6 +34,14 @@ public class AdminRoomsRecyclerViewAdaptor extends RecyclerView.Adapter<AdminRoo
     @Override
     public void onBindViewHolder(@NonNull AdminRoomsRecyclerViewAdaptor.ViewHolder holder, int position) {
         holder.textViewRoom.setText(localDatSet.get(position).toString());
+        holder.buttonAddSensor.setOnClickListener(view -> {
+            Room room = localDatSet.get(holder.getLayoutPosition());
+            new AddSensorDialogFragment(room).show(fragmentManager, "AddSensorDialogFragment");
+        });
+        holder.buttonRemoveSensor.setOnClickListener(view -> {
+            Room room = localDatSet.get(holder.getLayoutPosition());
+            new RemoveSensorDialogFragment(room).show(fragmentManager, "RemoveSensorDialogFragment");
+        });
     }
 
     @Override
