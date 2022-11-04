742afa2a0ff881077f78d9d1e5c542fcc1ec12c0
SamsonKaller <samson.kaller@gmail.com>
2022-11-04 17:51:31 -0400

    AdminRoomsActivity: made adminEmail a class attribute and added toast to Add/Remove rooms buttons

--- app/src/main/java/com/example/finalproject/views/AdminRoomsActivity.java
+++ app/src/main/java/com/example/finalproject/views/AdminRoomsActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.finalproject.R;
 import com.example.finalproject.models.Room;
@@ -19,6 +20,7 @@ import java.util.List;
 
 public class AdminRoomsActivity extends AppCompatActivity {
 
+    private String adminEmail;
     private RecyclerView recycler;
     private AdminRoomsRecyclerViewAdaptor adaptor;
 
@@ -28,7 +30,7 @@ public class AdminRoomsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_rooms);
 
         Intent intent = getIntent();
-        String adminEmail = intent.getStringExtra(getString(R.string.Extra_adminEmail));
+        adminEmail = intent.getStringExtra(getString(R.string.Extra_adminEmail));
 
         Toolbar toolbar = findViewById(R.id.adminRoomsActivityToolbar);
         setSupportActionBar(toolbar);
@@ -51,9 +53,11 @@ public class AdminRoomsActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_add_room) {
+            Toast.makeText(this, "Clicked on Add Room for admin: " + adminEmail, Toast.LENGTH_SHORT).show();
             return true;
         }
         else if (id == R.id.action_remove_room) {
+            Toast.makeText(this, "Clicked on Remove Room for admin: " + adminEmail, Toast.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
