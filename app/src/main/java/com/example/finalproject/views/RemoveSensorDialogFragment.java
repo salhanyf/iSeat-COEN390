0fc7637ea8acab6cf7a1d8f8177ccc17197236b3
SamsonKaller <samson.kaller@gmail.com>
2022-11-09 21:30:05 -0500

    Refactor: Converted RemoveSensorDialogFragment.java to use FirebaseDatabaseHelper.java

--- app/src/main/java/com/example/finalproject/views/RemoveSensorDialogFragment.java
+++ app/src/main/java/com/example/finalproject/views/RemoveSensorDialogFragment.java
@@ -18,25 +18,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.finalproject.R;
+import com.example.finalproject.controllers.FirebaseDatabaseHelper;
 import com.example.finalproject.models.Room;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.example.finalproject.models.Sensor;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RemoveSensorDialogFragment extends DialogFragment {
     private final Room room;
-    private List<String> list;
+    FirebaseDatabaseHelper db;
     private TextView textViewNoneAssigned;
     private ListView listViewSensorsInRoom;
-
-    private Query q;
-    private ValueEventListener listener;
 
     public RemoveSensorDialogFragment(Room room) {
         super();
@@ -47,76 +41,75 @@ public class RemoveSensorDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialogfragment_remove_sensor, container);
-
-        ((TextView) view.findViewById(R.id.textViewForRoom)).setText(String.format("%s\n%s", getString(R.string.RemoveSensor_TextView_ForRoom), room));
-        textViewNoneAssigned = view.findViewById(R.id.textViewNoneAssigned);
-        listViewSensorsInRoom = view.findViewById(R.id.listViewSensorsInRoom);
-
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-
-        q = ref.child("sensors").orderByChild("roomID").equalTo(Integer.parseInt(room.getKey()));
-        listener = q.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                list = new ArrayList<>();
-
-                for (DataSnapshot snap : snapshot.getChildren()) {
-                    list.add(snap.getKey());
-                }
-
-                if (list.isEmpty()) {
-                    textViewNoneAssigned.setVisibility(View.VISIBLE);
-                    listViewSensorsInRoom.setVisibility(View.GONE);
-                } else {
-                    listViewSensorsInRoom.setVisibility(View.VISIBLE);
-                    textViewNoneAssigned.setVisibility(View.GONE);
-
-                    listViewSensorsInRoom.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, list) {
-                        @NonNull
-                        @Override
-                        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                            TextView textView = new TextView(getContext());
-                            textView.setGravity(Gravity.CENTER);
-                            textView.setMinHeight(50);
-                            textView.setPadding(10, 10, 10, 10);
-                            textView.setTextColor(getContext().getColor(R.color.black));
-                            textView.setOnClickListener(view -> (new ConfirmDialog(getContext(), list.get(position))).show());
-                            return super.getView(position, textView, parent);
-                        }
-
-                        class ConfirmDialog extends AlertDialog.Builder {
-                            public ConfirmDialog(Context context, String sensorKey) {
-                                super(context);
-                                setTitle(room.toString());
-                                setMessage("Confirm removing sensor " + sensorKey + " from room.");
-                                setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.cancel());
-                                setPositiveButton("Confirm", ((dialogInterface, i) -> {
-                                    // get reference to Firebase for the sensor's roomID
-                                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("sensors/" + sensorKey + "/roomID");
-                                    // update the sensors roomID to this room
-                                    ref.setValue(0);
-                                    // print success toast and dismiss dialog fragment
-                                    Toast.makeText(getContext(), "Remove " + sensorKey + " from " + room.toString(), Toast.LENGTH_SHORT).show();
-                                    dismiss();
-                                }));
-                            }
-                        }
-                    });
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(getContext(), "Error reading Sensors: " + error.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
-
+        setupUI(view);
+        db = new FirebaseDatabaseHelper();
+        db.listenToSensorsRoom(room.getKey(), new UpdateSensorsListView());
         return view;
     }
 
     @Override
     public void onDismiss(@NonNull DialogInterface dialog) {
-        q.removeEventListener(listener);
         super.onDismiss(dialog);
+        db.removeSensorsListeners();
+    }
+
+    private void setupUI(View view) {
+        ((TextView) view.findViewById(R.id.textViewForRoom)).setText(String.format("%s\n%s", getString(R.string.RemoveSensor_TextView_ForRoom), room));
+        textViewNoneAssigned = view.findViewById(R.id.textViewNoneAssigned);
+        listViewSensorsInRoom = view.findViewById(R.id.listViewSensorsInRoom);
+    }
+
+    private class UpdateSensorsListView implements FirebaseDatabaseHelper.SensorDataChange {
+        @Override
+        public void dataUpdated(List<Sensor> sensors) {
+            if (sensors.isEmpty()) {
+                textViewNoneAssigned.setVisibility(View.VISIBLE);
+                listViewSensorsInRoom.setVisibility(View.GONE);
+            } else {
+                listViewSensorsInRoom.setVisibility(View.VISIBLE);
+                textViewNoneAssigned.setVisibility(View.GONE);
+                // add ArrayAdaptor with method overrides for Sensor item in listview
+                listViewSensorsInRoom.setAdapter(new SensorsInRoomArrayAdaptor(getContext(), sensors));
+            }
+        }
+    }
+
+    private class SensorsInRoomArrayAdaptor extends ArrayAdapter<Sensor> {
+        private List<Sensor> sensors;
+
+        public SensorsInRoomArrayAdaptor(Context context, List<Sensor> sensors) {
+            super(context, android.R.layout.simple_list_item_1, sensors);
+            this.sensors = sensors;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            TextView textView = new TextView(getContext());
+            textView.setGravity(Gravity.CENTER);
+            textView.setMinHeight(50);
+            textView.setPadding(10, 10, 10, 10);
+            textView.setTextColor(getContext().getColor(R.color.black));
+            textView.setOnClickListener(view -> (new ConfirmDialog(getContext(), sensors.get(position).getKey())).show());
+            return super.getView(position, textView, parent);
+        }
+    }
+
+    private class ConfirmDialog extends AlertDialog.Builder {
+        public ConfirmDialog(Context context, String sensorKey) {
+            super(context);
+            setTitle(room.toString());
+            setMessage("Confirm removing sensor " + sensorKey + " from room.");
+            setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.cancel());
+            setPositiveButton("Confirm", ((dialogInterface, i) -> {
+                // get reference to Firebase for the sensor's roomID
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("sensors/" + sensorKey + "/roomID");
+                // update the sensors roomID to this room
+                ref.setValue(0);
+                // print success toast and dismiss dialog fragment
+                Toast.makeText(getContext(), "Remove " + sensorKey + " from " + room.toString(), Toast.LENGTH_SHORT).show();
+                dismiss();
+            }));
+        }
     }
 }
