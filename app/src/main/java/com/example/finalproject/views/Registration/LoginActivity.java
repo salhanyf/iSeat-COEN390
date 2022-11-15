df8e46602458a1a1b921fa70373c12be69af0576
SamsonKaller <samson.kaller@gmail.com>
2022-11-15 18:13:40 -0500

    Refactor: added check error method, finish() in appropriate places. Page immediate closes if user already logged in

--- app/src/main/java/com/example/finalproject/views/Registration/LoginActivity.java
+++ app/src/main/java/com/example/finalproject/views/Registration/LoginActivity.java
@@ -1,21 +1,15 @@
 package com.example.finalproject.views.Registration;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.finalproject.R;
-import com.example.finalproject.views.MainActivity;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -30,49 +24,52 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            Toast.makeText(this, "User " + auth.getCurrentUser().getEmail() + " already signed in!", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
         loginEmail = findViewById(R.id.emailLogin);
         loginPassword = findViewById(R.id.passwordLogin);
         loginButton = findViewById(R.id.loginButton);
         signupRedirectButton = findViewById(R.id.signupRedirect);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = loginEmail.getText().toString();
-                String password = loginPassword.getText().toString();
+        signupRedirectButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, RegisterActivity.class));
+            finish();
+        });
 
-                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    if (!password.isEmpty()) {
-                        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                            @Override
-                            public void onSuccess(AuthResult authResult) {
-                                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                                finish();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+        loginButton.setOnClickListener(v -> {
+            String email = loginEmail.getText().toString();
+            String password = loginPassword.getText().toString();
+
+            if (!checkErrors(email, password)) {
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        finish();
                     } else {
-                        loginPassword.setError("Please Enter Password");
+                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                     }
-                } else if (email.isEmpty()) {
-                    loginEmail.setError("Please Enter Email");
-                } else {
-                    loginEmail.setError("Please Enter Valid Email");
-                }
-
+                });
             }
         });
+    }
 
-        signupRedirectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity( new Intent(LoginActivity.this, RegisterActivity.class));
-            }
-        });
+    private boolean checkErrors(String email, String password) {
+        boolean err = false;
+        if (email.isEmpty()) {
+            loginEmail.setError("Please Enter Email");
+            err = true;
+        } else if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            loginEmail.setError("Please Enter Valid Email");
+            err = true;
+        }
+        if (password.isEmpty()) {
+            loginPassword.setError("Please Enter Password");
+            err = true;
+        }
+        return err;
     }
 }
