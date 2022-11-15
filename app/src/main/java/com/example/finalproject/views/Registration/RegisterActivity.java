82d4a3b38bdd1c9e430e6a7a821df1fb26a636eb
SamsonKaller <samson.kaller@gmail.com>
2022-11-15 17:41:27 -0500

    RegisterActivity: added entry to Firebase Database if user creates admin account.

--- app/src/main/java/com/example/finalproject/views/Registration/RegisterActivity.java
+++ app/src/main/java/com/example/finalproject/views/Registration/RegisterActivity.java
@@ -1,6 +1,5 @@
 package com.example.finalproject.views.Registration;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,11 +11,8 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.example.finalproject.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
     private final String ADMIN_PASSWORD = "admin";
@@ -25,7 +21,6 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText signupUsername, signupEmail, signupPassword, signupPasswordRepeat, adminCodeSignup;
     private RadioButton anAdmin, notAnAdmin;
     private Button signupCreateAccountButton, loginRedirectButton;
-//    private String accountType = "user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +45,8 @@ public class RegisterActivity extends AppCompatActivity {
         loginRedirectButton = findViewById(R.id.loginRedirect);
 
         loginRedirectButton.setOnClickListener(v -> {
-            finish();
             startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            finish();
         });
 
         signupCreateAccountButton.setOnClickListener(v -> {
@@ -61,8 +56,27 @@ public class RegisterActivity extends AppCompatActivity {
             String passwordRepeat = signupPasswordRepeat.getText().toString().trim();
             String adminPasswordEntered = adminCodeSignup.getText().toString();
 
-            if (!checkErrors(username, email, password, passwordRepeat, adminPasswordEntered))
-                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new RegisterSuccessOnCompleteListener(email, password, anAdmin.isChecked()));
+            if (!checkErrors(username, email, password, passwordRepeat, adminPasswordEntered)) {
+
+                if (anAdmin.isChecked())
+                    FirebaseDatabase.getInstance().getReference("admins").push().setValue(email);
+
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                    task -> {
+                        if (task.isSuccessful()) {
+                            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(t -> {
+                                if (t.isSuccessful())
+                                    Toast.makeText(RegisterActivity.this, "User Registration & Login Success", Toast.LENGTH_SHORT).show();
+                                else
+                                    Toast.makeText(RegisterActivity.this, "User Registration Success but Login Failed", Toast.LENGTH_SHORT).show();
+                                finish();
+                            });
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "User Registration Failed, Try Again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                );
+            }
         });
     }
 
@@ -96,62 +110,15 @@ public class RegisterActivity extends AppCompatActivity {
         //check which radio button was clicked
         switch(view.getId()) {
             case R.id.adminYes:
-                if (checked)
+//                if (checked)
                     //Toast.makeText(RegisterActivity.this, "Admin",Toast.LENGTH_SHORT).show();
-//                    accountType = "admin";
                     adminCodeSignup.setVisibility(View.VISIBLE);
                     break;
             case R.id.adminNo:
-                if (checked)
+//                if (checked)
                     //Toast.makeText(RegisterActivity.this, "Not Admin",Toast.LENGTH_SHORT).show();
-//                    accountType = "user";
                     adminCodeSignup.setVisibility(View.GONE);
                     break;
-        }
-    }
-
-    private class RegisterSuccessOnCompleteListener implements OnCompleteListener<AuthResult> {
-        final String email, password;
-        final boolean adminFlag;
-
-        RegisterSuccessOnCompleteListener(String email, String password, boolean adminFlag) {
-            super();
-            this.email = email;
-            this.password = password;
-            this.adminFlag = adminFlag;
-        }
-
-        @Override
-        public void onComplete(@NonNull Task<AuthResult> task) {
-            if (task.isSuccessful()) {
-                if (adminFlag) {
-                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(t -> {
-                        if (t.isSuccessful()) {
-                            Toast.makeText(RegisterActivity.this, "User Registration & Login Success", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(RegisterActivity.this, WelcomeActivity.class));
-//                            finishAffinity();
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "User Registration Success but Login Failed", Toast.LENGTH_SHORT).show();
-//                            finishAffinity();
-                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                        }
-                    });
-                } else {
-                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(t -> {
-                        if (t.isSuccessful()) {
-                            Toast.makeText(RegisterActivity.this, "User Registration & Login Success", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(RegisterActivity.this, WelcomeActivity.class));
-//                            finish();
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "User Registration Success but Login Failed", Toast.LENGTH_SHORT).show();
-//                            finishAffinity();
-                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                        }
-                    });
-                }
-            } else {
-                Toast.makeText(RegisterActivity.this, "User Registration Failed, Try Again", Toast.LENGTH_SHORT).show();
-            }
         }
     }
 }
